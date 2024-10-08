a9771babad3d103107ca63af30c4892b3675bdea
jangseungri <jangseungri3058@gmail.com>
2024-10-08 20:55:08 +0900

    Title: 다양하게 출력, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241008/다양하게 출력/print-in-variety.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String a = "Total days in Year";
+        int day = 365;
+        String b = "Circumference rate";
+        double pi = 3.1415926535;
+
+        System.out.println(a);
+        System.out.println(day);
+        System.out.println(b);
+        System.out.println(pi);
+    }
+}
