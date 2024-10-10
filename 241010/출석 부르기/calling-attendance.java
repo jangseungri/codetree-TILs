23e0373bf3120660120e3b13844eb1d2175f573b
jangseungri <jangseungri3058@gmail.com>
2024-10-10 19:57:03 +0900

    Title: 출석 부르기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241010/출석 부르기/calling-attendance.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if(number == 1) {
+            System.out.print("John");
+        } else if(number == 2) {
+            System.out.print("Tom");
+        } else if(number == 3) {
+            System.out.print("Paul");
+        } else {
+            System.out.print("Vacancy");
+        }
+    }
+}
