501c025ff1e23e8d9926a18779dffd346f439c79
jangseungri <jangseungri3058@gmail.com>
2024-10-14 09:37:44 +0900

    Title: a/b 출력, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241014/a/b 출력/a-divide-b.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if((a / b) == 0) {
+            System.out.print(a / b + ".");
+            a *= 10;
+        }
+
+        for(int i = 1; i < 21; i++) {
+            System.out.print(a / b);
+            a = a % b;
+            a *= 10;
+        }
+    }
+}
