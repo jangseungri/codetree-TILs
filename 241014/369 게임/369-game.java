3acdba691a1de5850eef5733d29f1f210545792b
jangseungri <jangseungri3058@gmail.com>
2024-10-14 10:27:42 +0900

    Title: 369 게임, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/369 게임/369-game.java
+++ 241014/369 게임/369-game.java
@@ -7,27 +7,14 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++) {
-            int current = i;
-            boolean isClap = false;
-
-            if(current % 3 == 0) {
-                isClap = true;
-            } else {
-                while(current > 0) {
-                    int digit = current % 10;
-                    if(digit == 3 || digit == 6 || digit == 9) {
-                        isClap = true;
-                        break;
-                    }
-                    current /= 10;
-                }
-            }
-
-            if(isClap) {
-                System.out.print(0 + " ");
-            } else {
+            if(i % 3 == 0)
+                System.out.print("0 ");
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                System.out.print("0 ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                System.out.print("0 ");
+            else
                 System.out.print(i + " ");
-            }
         }
     }
 }
