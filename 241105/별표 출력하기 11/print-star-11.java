4d2cca49421274edece11d0c10421856c593646a
jangseungri <jangseungri3058@gmail.com>
2024-11-05 16:08:36 +0900

    Title: 별표 출력하기 11, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241105/별표 출력하기 11/print-star-11.java
+++ 241105/별표 출력하기 11/print-star-11.java
@@ -8,7 +8,7 @@ public class Main {
 
         for(int i = 0; i < n * 2 + 1; i++) {
             for(int j = 0; j < n * 2 + 1; j++) {
-                if(i == 0 || i == 2 || i == 4 || i == 6 || j == 0 || j == 2 || j == 4 || j == 6) {
+                if(i % 2 == 0 || j % 2 == 0) {
                         System.out.print("* ");
                 }
                 else 
